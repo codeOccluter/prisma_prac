d2385a28575400252f084af56f91ff28f1cb936b
kihoon <coderkihoon@gmail.com>
2024-12-09 17:51:26 +0900

    i18n, footer add

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,15 +1,21 @@
+"use client"
+
 import React from "react"
 import styles from "../../../css/layout/footer.module.css"
+import "../../i18n"
+import { useTranslation } from "react-i18next"
 
 const Footer = () => {
+
+    const { t } = useTranslation()
 
     return (
         <footer className={styles.footer}>
             <div className={styles.container}>
                 <div className={styles.info}>
                     <p>
-                        <span>Authorize : kihunism</span><br/>
-                        <span>Copyright 2024. kihunism. All Rights Reserved.</span>
+                        <span>{t('authorize')}</span><br/>
+                        <span>{t('copyright')}</span>
                     </p>
                 </div>
             </div>
